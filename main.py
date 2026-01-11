12b9b336da18b9eca3070a41c47e6a855e56a9ef
liujiajun <liujiajun1234@gmail.com>
2026-01-11 18:32:02 +0800

    data set

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
 
-# 创建一个 2D 张量
-tensor = torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.float32)
 
-# 张量的属性
-print("Tensor:\n", tensor)
-print("Shape:", tensor.shape)  # 获取形状
-print("Size:", tensor.size())  # 获取形状（另一种方法）
-print("Data Type:", tensor.dtype)  # 数据类型
-print("Device:", tensor.device)  # 设备
-print("Dimensions:", tensor.dim())  # 维度数
-print("Total Elements:", tensor.numel())  # 元素总数
-print("Requires Grad:", tensor.requires_grad)  # 是否启用梯度
-print("Is CUDA:", tensor.is_cuda)  # 是否在 GPU 上
-print("Is Contiguous:", tensor.is_contiguous())  # 是否连续存储
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
 
-# 获取单元素值
-single_value = torch.tensor(42)
-print("Single Element Value:", single_value.item())
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
 
-# 转置张量
-tensor_T = tensor.T
-print("Transposed Tensor:\n", tensor_T)
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
